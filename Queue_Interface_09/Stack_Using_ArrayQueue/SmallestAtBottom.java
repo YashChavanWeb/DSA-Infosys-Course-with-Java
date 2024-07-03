995e67df836ffd0d4b3bcc14f5379d288c1e23c9
Yash Chavan <yashchavan4628@gmail.com>
2024-07-03 22:32:34 +0530

    all the exercises are done. Just assignments are remaining

---
+++ Queue_Interface_09/Stack_Using_ArrayQueue/SmallestAtBottom.java
@@ -0,0 +1,42 @@
+package Queue_Interface_09.Stack_Using_ArrayQueue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SmallestAtBottom {
+
+    public static Deque<Integer> changeSmallest(Deque<Integer> inputStack) {
+        Deque<Integer> tempStack = new ArrayDeque<>();
+
+        while (!inputStack.isEmpty()) {
+            int current = inputStack.pop();
+
+            // Transfer elements from tempStack back to inputStack as needed
+            while (!tempStack.isEmpty() && tempStack.peek() > current) {
+                inputStack.push(tempStack.pop());
+            }
+
+            // Push current element onto tempStack
+            tempStack.push(current);
+        }
+
+        return tempStack;
+
+    }
+
+    public static void main(String[] args) {
+
+        Deque<Integer> inputStack = new ArrayDeque<Integer>();
+        inputStack.push(10);
+        inputStack.push(8);
+        inputStack.push(5);
+        inputStack.push(12);
+        inputStack.push(5);
+
+        Deque<Integer> updatedStack = changeSmallest(inputStack);
+
+        System.out.println("Stack After Modification:");
+        for (Integer value : updatedStack)
+            System.out.println(value);
+    }
+}
